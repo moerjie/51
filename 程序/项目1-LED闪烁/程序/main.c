d556832c5acb38f9aae47747f1cf9a3adbe8dc8e
moerjie <3165109406@qq.com>
2023-08-25 18:31:53 +0800

    8.25

--- 程序/项目1-LED闪烁/程序/main.c
+++ 程序/项目1-LED闪烁/程序/main.c
@@ -1,5 +1,6 @@
 #include<reg51.h>
-sbit LED = P0^0;		  //LED接P0.0
+sbit LED_R = P0^0;		  //LED接P0.0
+sbit LED_B = P0^1;
 void delay(unsigned int x)			 //延时函数
 {
 	while(x--);
@@ -8,9 +9,13 @@ void main()							 //主函数
 {
 	while(1)
 	{
-	LED=0;									  //P0.0输出低电平，LED灭
-	delay(500000);							 //调用延时函数，延时一段时间
-	LED=1;									//P0.0输出高电平，LED亮
-	delay(500000);						   //调用延时函数，延时一段时间
+		LED_R=0;									  //P0.0输出低电平，LED灭
+		delay(500000);							 //调用延时函数，延时一段时间
+		LED_R=1;									//P0.0输出高电平，LED亮
+		delay(500000);						   //调用延时函数，延时一段时间
+		LED_B=0;
+		delay(500000);
+		LED_B=1;
+		delay(500000);
 	}
 }  
